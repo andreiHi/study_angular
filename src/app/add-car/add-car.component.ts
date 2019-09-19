cba24a51613dba4c1d1cf3f58a703730d28c50cd
Andrei Hincu <andreih1981@gmail.com>
2019-09-19 22:58:01 +0300

    Доступ к DOM элементам

--- src/app/add-car/add-car.component.ts
+++ src/app/add-car/add-car.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
 import {Car} from '../car/car';
 
 @Component({
@@ -12,7 +12,12 @@ export class AddCarComponent {
   year = 2017;
   @Output()carEmitter = new EventEmitter<Car>();
 
-  addCar() {
+  // @ts-ignore
+  @ViewChild('carYearInput') carYearInput: ElementRef;
+
+  addCar(name: HTMLInputElement) {
+    this.year = +this.carYearInput.nativeElement.value;
+    this.carName = name.value;
     this.carEmitter.emit(new Car(this.carName, this.year));
     this.carName  = '';
     this.year = 2017;
