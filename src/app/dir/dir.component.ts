e191cf82ce00d11983914759a0a2b9cfa4049b61
Andrei Hincu <andreih1981@gmail.com>
2019-09-23 11:13:16 +0300

    Директива ngSwitch

--- src/app/dir/dir.component.ts
+++ src/app/dir/dir.component.ts
@@ -8,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class DirComponent implements OnInit {
 
   headerText = 'Создание своей директивы';
+  items = [1, 2, 3, 4, 5];
+  current = 1;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClick(item: number) {
+    this.current = item;
+  }
 }
