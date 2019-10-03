f7b835d77eec0e4c4bd1cdb4584b7bcdd8b66526
Andrei Hincu <andreih1981@gmail.com>
2019-10-03 22:23:22 +0300

    Параметр Observe

--- src/app/cars.service.ts
+++ src/app/cars.service.ts
@@ -1,9 +1,9 @@
 import {Car} from './car/car';
 import {ConsoleService} from './console.service';
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpEventType, HttpParams} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
-import {catchError, delay} from 'rxjs/operators';
+import {catchError, delay, map, tap} from 'rxjs/operators';
 
 @Injectable() // говорит о том что в данный класс можно что либо инжектить
 export class CarsService {
@@ -16,14 +16,25 @@ export class CarsService {
     }
 
     getCars(): Observable<Car[]> {
-        return this.http.get<Car[]>('/cars')
+        let params = new HttpParams();
+        params = params.append('_limit', '4');
+        params = params.append('custom', 'anything');
+        return this.http.get<Car[]>('/cars',
+            {
+                params, // ередача параметров в url запроса
+                observe: 'response' // данный флаг позволяет получить целиком (хэдер статус и тд)ответ сервера
+            })
             .pipe(
+                map( response => {
+           //         console.log('Response ', response);
+                    return  response.body;
+                }),
                 delay(500),
                 catchError(error => {
                     this.consoleService.log(error.message);
                     return throwError(error);
                 })
-                );
+            );
 
     }
 
@@ -34,10 +45,25 @@ export class CarsService {
 
     changeBuy(car: Car, isSold: boolean): Observable<Car> {
         car.isSold = isSold;
-        return this.http.put<Car>(`/cars/${car.id}`, car);
+        return this.http.put<Car>(`/cars/${car.id}`, car,
+            {
+             responseType: 'json' // позволяет изменить тип возвращаемого значения по умолчаниэ это json
+            });
     }
 
-    delete(car: Car): Observable<void> {
-        return this.http.delete<void>(`/cars/${car.id}`);
+    delete(car: Car): Observable<any> {
+        return this.http.delete<void>(`/cars/${car.id}`, {
+            observe: 'events' // данный флаг позволяет отслеживать все события по отправке и получению запроса
+        }).pipe(
+            tap(event => { // позволяет получать события по шагово
+                console.log(event);
+                if (event.type === HttpEventType.Sent) {
+                    console.log('Send', event);
+                }
+                if (event.type === HttpEventType.Response) {
+                    console.log('response', event);
+                }
+            })
+        );
     }
 }
