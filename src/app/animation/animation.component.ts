cbcef2ee948816f72c2943b40b7c72fd50d04e65
Andrei Hincu <andreih1981@gmail.com>
2019-10-13 22:50:23 +0300

    Сложный переход

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -25,12 +25,37 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
           transition('start <=> end', animate(800)),
           transition('start => active', animate(400)),
           transition('active => end', animate(400))
+      ]),
+      trigger('multi', [
+          state('start', style({
+            width: '150px',
+            height: '150px',
+            border: '5px solid black'
+          })),
+          state('end', style({
+            width: '170px',
+            height: '170px',
+            background: 'blue'
+          })),
+          transition('start <=> end', [
+              style({
+                background: 'red'
+              }),
+              animate(1500, style({
+                background: 'yellow'
+                  })),
+              animate(1000, style({
+                width: '200px',
+                height: '200px',
+              }))
+          ])
       ])
   ]
 })
 export class AnimationComponent implements OnInit {
 
   clickedDiveState = 'start';
+  multiState = 'start';
 
   constructor() { }
 
